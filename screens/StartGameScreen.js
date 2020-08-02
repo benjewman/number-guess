8099d2d817a53799a5f83c3ec05aebe7aac6bbff
benjewman <bennyadave@gmail.com>
2020-08-02 16:58:31 -0400

    Create Card component to reuse

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+import Card from '../components/Card';
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Start a New Game!</Text>
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
                 <TextInput />
                 <View style={styles.buttonContainer}>
                     <Button title="Reset" onPress={() => {}}/>
                     <Button title="Confirm" onPress={() => {}}/>
                 </View>
-            </View>
+            </Card>
         </View>
     );
 };
@@ -30,15 +31,7 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center',
-        shadowColor: 'black',
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        backgroundColor: 'white',
-        elevation: 5,
-        padding: 20,
-        borderRadius: 10
+        alignItems: 'center'
     },
     buttonContainer: {
         flexDirection: 'row',
