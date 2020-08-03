9958a2b8c4986a64fd4a0c3058ba5013472afd28
benjewman <bennyadave@gmail.com>
2020-08-03 18:17:12 -0400

    Create fonts folder and load fonts in App component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -97,7 +97,8 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 20,
-        marginVertical: 10
+        marginVertical: 10,
+        fontFamily: 'open-sans-bold'
     },
     inputContainer: {
         width: 300,
