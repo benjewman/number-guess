e92a4861582f010dc1aab13c50ae9683ae7f0bbb
benjewman <bennyadave@gmail.com>
2020-08-04 23:36:47 -0400

    Adjust listContainer width in GameScreen with Dimensions object

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,7 +5,8 @@ import {
     StyleSheet, 
     Alert, 
     ScrollView, 
-    FlatList
+    FlatList,
+    Dimensions
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -112,12 +113,12 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 400,
         maxWidth: '90%'
     },
     listContainer: {
-        width: '60%',
+        width: Dimensions.get('window').width > 350 ? '60%' : '80%',
         flex: 1
     },
     list: {
